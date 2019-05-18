667201142c6e4f5518f18ffe8e1bbadafa1c5cee
Omar <ofjolivares@gmail.com>
2019-05-18 17:23:41 -0400

    Añadir archivo de Tarea Evaluada

--- interface/tarea01.py
+++ interface/tarea01.py
@@ -1,19 +1,23 @@
-import numpy as np # liberia para el manejo de los arreglos
-import matplotlib.pyplot as plt # libreria de manejo de los graficos
-from mpl_toolkits.mplot3d import Axes3D # libreria para proyectar graficos en 3D
-import tkinter as tk # libreria para menejar las interface grafica
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-
-def create_plot():
+def curva_de_ejemplo():
     """
-    La presente es una función de ejemplo de cómo projectar en un gráfico 3D una curva paramétrica
-    tomada desde https://matplotlib.org/gallery/mplot3d/lines3d.html
-    :return:
-    """
-    plt.rcParams['legend.fontsize'] = 12
+    Curva de Ejemplo que despliega una curva paramétrica en una ventana nueva
 
+    Integrantes:
+    - Omar Olivares Urrutia (@ofou)
+    :return: plot curve
+    """
     fig = plt.figure()
-    ax = Axes3D(fig)
+    ax = fig.add_subplot(111, projection='3d')
 
     # Prepare arrays x, y, z
     theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
@@ -25,8 +29,96 @@ def create_plot():
     ax.plot(x, y, z, label='Curva Paramétrica de Ejemplo')
     ax.legend()
 
-    plt.show()
+    fig.show()
 
+    # Añadir el plot de matplotlib
+    # fig = Figure(figsize=(5, 4), dpi=100)
+    # t = np.arange(0, 3, .01)
+    # fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+    #
+    # canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
+    # canvas.draw()
+    # canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    #
+    # toolbar = NavigationToolbar2Tk(canvas, root)
+    # toolbar.update()
+    # canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    # canvas.mpl_connect("key_press_event", on_key_press)
+
+    # If you put root.destroy() here, it will cause an error if the window is
+    # closed with the window manager.
+
+
+def helice_conica():
+    # añadir sus códigos aca
+    pass
+def helice_circular_1():
+    # añadir sus códigos aca
+    pass
+def helice_circular_2():
+    # añadir sus códigos aca
+    pass
+def curva_de_viviani():
+    # añadir sus códigos aca
+    pass
+def hipopoda_1():
+    # añadir sus códigos aca
+    pass
+def conica_de_papus():
+    # añadir sus códigos aca
+    pass
+def hipopoda_2():
+    # añadir sus códigos aca
+    pass
+def horoptera():
+    # añadir sus códigos aca
+    pass
+def hipopoda_3():
+    # añadir sus códigos aca
+    pass
 
 if __name__ == '__main__':
-    create_plot()
+    # Creación de Ventanas
+    root = tk.Tk()
+    root.wm_title("Proyecto de Fisica 2019/01")
+    root.geometry("800x600")
+
+    # Crear frame contenedor de los elementos
+    frame = tk.Frame(root)
+    frame.pack(padx=20, pady=20)
+    # Añadir titulo
+    label = tk.Label(frame, text="Curvas Paramétricas Famosas", height="2")
+    label.pack(fill=tk.X, expand=1)
+
+    # Cada grupo debe utilizar sus propia función
+    curva_de_ejemplo = tk.Button(master=frame, text="Curva de Ejemplo", command=curva_de_ejemplo)
+    curva_de_ejemplo.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+    curva_de_ejemplo = tk.Button(master=frame, text="Hélice Cónica", command=helice_conica)
+    curva_de_ejemplo.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+    curva_de_ejemplo = tk.Button(master=frame, text="Hélice Circular 1", command=helice_circular_1)
+    curva_de_ejemplo.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+    curva_de_ejemplo = tk.Button(master=frame, text="Hélice Circular 2", command=helice_circular_2)
+    curva_de_ejemplo.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+    curva_de_ejemplo = tk.Button(master=frame, text="Curva de Viviani", command=curva_de_ejemplo)
+    curva_de_ejemplo.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+    curva_de_ejemplo = tk.Button(master=frame, text="Hipopoda 1", command=hipopoda_1)
+    curva_de_ejemplo.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+    curva_de_ejemplo = tk.Button(master=frame, text="Hipopoda 2", command=hipopoda_2)
+    curva_de_ejemplo.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+    curva_de_ejemplo = tk.Button(master=frame, text="Hipopoda 3", command=hipopoda_3)
+    curva_de_ejemplo.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+    curva_de_ejemplo = tk.Button(master=frame, text="Conica de Papus", command=conica_de_papus)
+    curva_de_ejemplo.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+    curva_de_ejemplo = tk.Button(master=frame, text="Horoptera", command=horoptera)
+    curva_de_ejemplo.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+    tk.mainloop()
