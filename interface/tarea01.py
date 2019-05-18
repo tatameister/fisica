603169c4aa01c977c449dc15b4b995cc132245f8
Omar <ofjolivares@gmail.com>
2019-05-18 11:40:44 -0400

    Añadir ecuación paramétrica de ejemplo

---
+++ interface/tarea01.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tkinter as tk
+
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def create_plot():
+    """
+    La presente es una función de ejemplo de cómo projectar en un gráfico 3D una curva paramétrica
+    tomada desde https://matplotlib.org/gallery/mplot3d/lines3d.html
+    :return:
+    """
+    plt.rcParams['legend.fontsize'] = 12
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+
+    # Prepare arrays x, y, z
+    theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
+    z = np.linspace(-2, 2, 100)
+    r = z ** 2 + 1
+    x = r * np.sin(theta)
+    y = r * np.cos(theta)
+
+    ax.plot(x, y, z, label='Curva Paramétrica de Ejemplo')
+    ax.legend()
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    create_plot()
